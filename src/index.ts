718e0af642422ff53550522f2a1281f92b44f867
Khalil Mansouri <khalil_mansouri@hotmail.fr>
2021-03-07 12:25:59 +0100

    Implement counter extraction

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,46 @@
 import * as mongoose from "mongoose"
 import { Schema } from "mongoose"
 
+enum InitCounter {
+  YEALY = "yearly",
+  MONTHLY = "monthly",
+  DAILY = "daily"
+}
+
 export type Options = {
   field: string,
   prefix: string,
   format: string,
+  separator: string,
+  initCounter: "yearly" | "monthly" | "hourly"
+  digits: number
 }
 
-// const extractCounter = (options: Options, serial: string): string => {
-//   // INVOICE-YYYY/DD/COUNTER
-//   let { prefix, format } = options
 
-//   return ''
-// }
+export const addZeros = (counter: number, size: number) => {
+  let counterString = counter.toString();
+  while (counterString.length < size) counterString = "0" + counterString;
+  return counterString;
+}
+
+export const extractCounter = (options: Options, serial: string): string => {
+  let { prefix, format, separator, initCounter, digits } = options
+  let counter: string
+
+  if (serial !== null) {
+    counter = serial.split(separator).slice(-1).join(separator)
+    return addZeros(parseInt(counter) + 1, digits)
+    // switch (initCounter) {
+    //   case InitCounter.YEALY:
+    //     counter = AddZeros()
+    // }
+  } else
+    return addZeros(1, digits)
+}
 
 export const plugin = (schema: Schema, options: Options) => {
-  let { field, prefix, format } = options
-  let separator = "-"
+  let { field, prefix, format, separator } = options
+  // let separator = "-"
   let counter;
   schema.pre("save", async function (next) {
     let doc: any = this
@@ -26,16 +50,14 @@ export const plugin = (schema: Schema, options: Options) => {
     }
 
     // get the last inserted document
-    let lastDoc = await (<any>this).constructor.findOne({}).sort(field)
+    let lastDoc = await (<any>this).constructor.findOne({}).sort({ [`${field}`]: -1 })
+    let serial = lastDoc ? lastDoc[field] : null
+    // extractCounter(options, serial)
 
-    if (!lastDoc)
-      counter = "0001"
-    else
-      counter = "0002"
-
+    counter = extractCounter(options, serial)
 
     // retrive the last count
-    doc[field] = ''.concat(prefix, separator, counter)
+    doc[field] = ''.concat(prefix.toUpperCase(), separator, counter)
     next()
   })
 }
