1d3d3eeb4d2069039852072b57f246bb162918c0
Khalil Mansouri <khalil_mansouri@hotmail.fr>
2021-03-07 16:34:34 +0100

    Add more test cases

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,88 @@
 import * as mongoose from "mongoose"
 import { Schema } from "mongoose"
 
-enum InitCounter {
-  YEALY = "yearly",
+/**
+ * 
+ */
+export enum InitCounter {
+  YEARLY = "yearly",
   MONTHLY = "monthly",
-  DAILY = "daily"
+  DAILY = "daily",
+  HOURLY = "hourly"
 }
 
+/**
+ * 
+ */
 export type Options = {
   field: string,
   prefix: string,
   format: string,
   separator: string,
-  initCounter: "yearly" | "monthly" | "hourly"
+  initCounter: "yearly" | "monthly" | "daily" | "hourly"
   digits: number
 }
 
-
+/**
+ * 
+ * @param counter 
+ * @param size 
+ */
 export const addZeros = (counter: number, size: number) => {
   let counterString = counter.toString();
   while (counterString.length < size) counterString = "0" + counterString;
   return counterString;
 }
 
+/**
+ * 
+ * @param options 
+ * @param serial 
+ */
 export const extractCounter = (options: Options, serial: string): string => {
-  let { prefix, format, separator, initCounter, digits } = options
+  let { prefix, separator, initCounter, digits } = options
   let counter: string
 
   if (serial !== null) {
     counter = serial.split(separator).slice(-1).join(separator)
+
+    let chunks = serial.split(separator)
+    // if (!prefix && !chunks.length) {}
+    switch (initCounter) {
+      case InitCounter.YEARLY:
+        let currentYear = new Date().getFullYear().toString()
+        let year = chunks[chunks.length - 2]
+        if (currentYear !== year)
+          return addZeros(1, digits)
+        break
+      case InitCounter.MONTHLY:
+        let currentMonth = addZeros(new Date().getMonth() + 1, 2)
+        let month = chunks[chunks.length - 2]
+        if (currentMonth !== month)
+          return addZeros(1, digits)
+        break
+      case InitCounter.DAILY:
+        let currentDay = addZeros(new Date().getDate(), 2)
+        // console.log({ currentDay })
+        let day = chunks[chunks.length - 2]
+        // console.log({ day })
+        if (currentDay !== day)
+          return addZeros(1, digits)
+        break
+    }
+
     return addZeros(parseInt(counter) + 1, digits)
-    // switch (initCounter) {
-    //   case InitCounter.YEALY:
-    //     counter = AddZeros()
-    // }
   } else
     return addZeros(1, digits)
 }
 
+/**
+ * 
+ * @param schema 
+ * @param options 
+ */
 export const plugin = (schema: Schema, options: Options) => {
-  let { field, prefix, format, separator } = options
-  // let separator = "-"
+  let { field, prefix, separator, initCounter } = options
   let counter;
   schema.pre("save", async function (next) {
     let doc: any = this
@@ -52,12 +94,26 @@ export const plugin = (schema: Schema, options: Options) => {
     // get the last inserted document
     let lastDoc = await (<any>this).constructor.findOne({}).sort({ [`${field}`]: -1 })
     let serial = lastDoc ? lastDoc[field] : null
-    // extractCounter(options, serial)
 
     counter = extractCounter(options, serial)
 
     // retrive the last count
-    doc[field] = ''.concat(prefix.toUpperCase(), separator, counter)
+    let dating;
+    switch (initCounter) {
+      case InitCounter.YEARLY:
+        dating = new Date().getFullYear().toString()
+      case InitCounter.MONTHLY:
+        dating = [new Date().getFullYear().toString(), addZeros(new Date().getMonth() + 1, 2)].join(separator)
+      case InitCounter.DAILY:
+        dating = [new Date().getFullYear().toString(), addZeros(new Date
+          ().getMonth() + 1, 2), addZeros(new Date().getDate(), 2)].join(separator)
+    }
+    let t = []
+    if (prefix) t.push(prefix)
+    if (dating) t.push(dating)
+    t.push(counter)
+    doc[field] = t.join(separator)
+    // console.log(doc[field])
     next()
   })
 }
---
+++ src/test/helerFunctions.test.ts
@@ -0,0 +1,39 @@
+import { expect } from "chai";
+const { addZeros, extractCounter, InitCounter } = require('../')
+
+let options = { field: 'serial', prefix: "Invoice", format: "PREFIX", separator: "-", digits: 5, initCounter: "daily" }
+
+describe('Helper-functions', function () {
+  it('should add lead zeros to integer number', () => {
+    let count = 3
+    let size = 5
+    let ret = addZeros(count, size)
+    expect(ret).to.equal('00003')
+
+    count = 300
+    size = 6
+    ret = addZeros(count, size)
+    expect(ret).to.equal('000300')
+
+  })
+
+  it('should extract and increment counter number daily', () => {
+    let serial = ["INVOICE", "2021", "03", "07", "00301"].join(options.separator)
+    let ret = extractCounter({ ...options, initCounter: InitCounter.DAILY }, serial)
+    expect(ret).to.equal('00302')
+  })
+
+  it('should extract and increment counter number monthly', () => {
+    let serial = ["INVOICE", "2021", "03", "00301"].join(options.separator)
+    let ret = extractCounter({ ...options, initCounter: "monthly" }, serial)
+    expect(ret).to.equal('00302')
+  })
+
+  it('should extract and increment counter number yearly', () => {
+    let serial = ["INVOICE", "2021", "00301"].join(options.separator)
+    // console.log({ ...options, initCounter: "yearly" })
+    let ret = extractCounter({ ...options, initCounter: "yearly" }, serial)
+    expect(ret).to.equal('00302')
+  })
+})
+
